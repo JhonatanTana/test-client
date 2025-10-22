454c88301fd96bd5aaefe075a87ce05b72560634
Jhonatan Souza Tana <jhonatan.tana@alunos.unis.edu.br>
2025-10-22 13:53:20 -0300

    att

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Configura os caminhos para todos os arquivos que usam classes Tailwind
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
